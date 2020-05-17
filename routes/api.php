16ef049f3fd09919613950a6ae8f285b422f4da9
zubyr <zubyrbutt@gmail.com>
2020-05-17 14:48:20 +0500

    Route error fix

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products', 'ProductController@index');
+Route::resource('products', 'ProductController');
